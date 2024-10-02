40cc500cd3d0eb5ccb427885577938ae02deb220
Luciana Henaut <32716405+luciana-henaut@users.noreply.github.com>
2024-10-02 08:54:40 +0100

    Created list account test case

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -50,6 +50,16 @@ class TestAccountService(TestCase):
         """Runs once after each test case"""
         db.session.remove()
 
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+
     ######################################################################
     #  H E L P E R   M E T H O D S
     ######################################################################
